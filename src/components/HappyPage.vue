3ca251696244ef7622914964b193bf5ce1a72311
unknown <suresh.rsisoft@gmail.com>
2025-02-07 14:43:14 +0530

    added the happy page

---
+++ src/components/HappyPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <H1>Happy Page</H1>
+  </div>
+</template>
+<script></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HappyPage from "@/components/HappyPage.vue";
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <HappyPage />
   </main>
 </template>
